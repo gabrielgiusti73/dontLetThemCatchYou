f36a18efd35c9e57a036b7d7472e325f0496a9eb
gabrielgiusti73 <57575954+gabrielgiusti73@users.noreply.github.com>
2020-12-11 18:09:37 -0300

    Update Inicializador.java

--- src/main/java/juego/Inicializador.java
+++ src/main/java/juego/Inicializador.java
@@ -1,13 +1,21 @@
 package juego;
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Inicializador {
-public static int reiniciaJuego = -1;
-	
+	public static int reiniciaJuego = -1;
+
 	public static void main(String[] args) {
-		
+		String nombresonido = "/dontLetThemCatchYou-main/src/main/resources/sonidos/backgroundMusic.wav";
+		ReproducirSonido(nombresonido);
 		JOptionPane.showMessageDialog(null, "¿Preparados?");
 		System.setProperty("sun.java2d.opengl", "true");
 		JFrame ventana = new JFrame("Don't let them catch you");
@@ -21,7 +29,7 @@ public static int reiniciaJuego = -1;
 
 		while (true) {
 			if (Juego.juegoFinalizado) {	
-				reiniciaJuego = JOptionPane.showConfirmDialog(null, "Oh no! Te atraparon! Volver a jugar?", "¡ Game Over !", JOptionPane.YES_NO_OPTION);
+				reiniciaJuego = JOptionPane.showConfirmDialog(null, "Te atraparon Mauricio! Volver a jugar?", "¡ Game Over !", JOptionPane.YES_NO_OPTION);
 				if (reiniciaJuego == 0) {	
 					reiniciaValores();
 				} else if (reiniciaJuego == 1) {
@@ -34,7 +42,7 @@ public static int reiniciaJuego = -1;
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
+
 				if (Juego.pierdeIntentoVida == true) {
 					JOptionPane.showMessageDialog(null, "Cuidado!! Te quedan: " + Juego.intentosVidas + " intentos");
 					Juego.pierdeIntentoVida = false;
@@ -46,7 +54,7 @@ public static int reiniciaJuego = -1;
 			}
 		}
 	}
-	
+
 	public static void reiniciaValores() {
 		Juego.juegoFinalizado = false;
 		Enemigo.auxiliarX = -4;
@@ -55,6 +63,17 @@ public static int reiniciaJuego = -1;
 		Juego.nivel = 1;
 		reiniciaJuego = -1;
 		Enemigo.inicialX = 1300;
+	}
+
+	public static void ReproducirSonido(String nombreSonido){
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.loop(reiniciaJuego);;
+		} catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+			System.out.println("Error al reproducir el sonido.");
+		}
 	}
 
 }
